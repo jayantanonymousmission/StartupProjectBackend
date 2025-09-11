c4735a1429dd126bcb6d8a62723082b0c4b44ee8
Jayant <jayant62644@gmail.com>
2025-09-11 08:34:44 +0530

    add one get request for testing

--- src/utils/emailVerification/emailverify.js
+++ src/utils/emailVerification/emailverify.js
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer';
 import variables from '../../storage/env/envConstants.js';
 
 //function handler for sending email
-const sendEmail=async(gmail,subject,text)=>{
+const sendEmail=async(email,subject,text)=>{
     const transport=nodemailer.createTransport({
         service:gmail,
         auth:{
@@ -15,18 +15,10 @@ const sendEmail=async(gmail,subject,text)=>{
     });
     await transport.sendMail({
         from:variables.userMail,
-        to:gmail,
+        to:email,
         subject:subject,
         text:text,
     });
-    transport.verify((err, success) => {
-  if (err) {
-    console.error("SMTP Error:", err);
-  } else {
-    console.log("SMTP Ready:", success);
-  }
-});
-
 }
 
 //export the functions or class for using functionality globally
