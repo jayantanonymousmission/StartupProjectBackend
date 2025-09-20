10fc00d93554bc271b86b3ad9d0d6b5ef5175fd4
Jayant <jayant62644@gmail.com>
2025-09-20 15:33:43 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -2,14 +2,12 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import express from "express";
+import app from "./app.js";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import authRoutes from "./src/routes/authRoutes/authRoute.js";
 import variables from "./src/storage/env/envConstants.js";
-
-const app = express();
 
 // ✅ CORS middleware
 app.use(cors({
@@ -19,9 +17,6 @@ app.use(cors({
 // Security + logging
 app.use(helmet({ crossOriginResourcePolicy: false }));
 app.use(morgan("dev"));
-
-// JSON parser
-app.use(express.json());
 
 // Routes
 app.use("/auth", authRoutes);
